9a796aca4063ca52acee7e061a37a140f91b3c67
daisuke sasaki <gerund25495641@gmail.com>
2021-02-23 09:24:14 +0900

    test

--- js/main.js
+++ js/main.js
@@ -7,6 +7,25 @@ console.clear();
     const year = 2020;
     const month = 4;  //５月
 
+   function getCalendarHead(){
+       const dates = [];
+       const d = new Date(year, month, 0).getDate();
+       const n = new Date(year, month, 1).getDay();
+
+
+       for (let i = 0; i < n; i++){
+           dates.unshift({
+              date: d - i,
+              isToday:false,
+              isDisabled:true,
+           });
+       }
+       console.log(dates);
+   }
+
+
+
+
     function getCalendarBody(){
         const dates = []; //date: 日付, day :曜日
         const lastDate = new Date(year, month + 1, 0).getDate();
@@ -21,5 +40,6 @@ console.clear();
         console.log(dates);
     }
 
-    getCalendarBody()
+    getCalendarHead()
+    // getCalendarBody()
 }
